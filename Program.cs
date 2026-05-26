0a5996a597f5f29572108644c1f9b969cfb777d2
Octavio Selis <octavioselis12@gmail.com>
2026-05-25 22:34:50 -0300

    Ejercicio 1 completado

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("Valor de a:" + a);
+Console.WriteLine("Valor de b:" + b);
+
+string numString = Console.ReadLine();
+
+int num,invertido=0;
+
+if(int.TryParse(numString, out num))
+{
+    if(num > 10)
+    {
+        while (num >= 10)
+        {
+            invertido = num%10;
+            num /= 10;
+            Console.Write(invertido);
+        }
+        Console.Write(num);
+    }
+    else
+    {
+        Console.Write(num);
+    }
+    
+}
+
+
